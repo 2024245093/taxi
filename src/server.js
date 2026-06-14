aaf9ed2a749aff8c1a8119f3b04602bb4b5b5b5f
김규민 <2024245093@yonsei.ac.kr>
2026-06-14 17:23:58 +0900

    Use environment variable for server port

--- src/server.js
+++ src/server.js
@@ -11,7 +11,8 @@ const { Server } = require('socket.io');
 const app        = express();
 const httpServer = http.createServer(app);
 const io         = new Server(httpServer);
-const PORT       = 5000;
+const PORT = process.env.PORT || 5000;
+httpServer.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
